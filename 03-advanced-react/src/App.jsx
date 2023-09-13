18cfc40bc80d8bdda7bd934c21ffbc856b5ad71f
sabmus <sabmus8@gmail.com>
2023-09-13 13:44:15 -0300

    first commit

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,6 +1,8 @@
+import Starter from './tutorial/06-forms/starter/01-controlled-inputs';
+
 function App() {
   return (
-    <div className='container'>
+    <div className="container">
       <h2>Advanced React</h2>
     </div>
   );
